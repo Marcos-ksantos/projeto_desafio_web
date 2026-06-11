4086e3b29412b59ec234dd38afdc277c4562c071
Tarde Cetafaju <Tarde.Cetafaju@labfies.org.br>
2026-06-11 15:57:21 -0300

    Adicionando variáveis ao js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
 console.log("Teste")
+console.log("Marcos e Lorena")
+
+//COMENTÁRIOM DE LINHA ÚNICA
+
+/*COMENTÁRIO DE  VÁRIAS LINHAS 
+*/
+//CRIANDO VARIÁVEIS
+let num = 50
+var num2 = 180
+const num3 = 200
+
+console.log(num)
+console.log(num2)
+console.log(num3)
+
+if (true){
+    let num4 = 420
+    var num5 = 180
+
+    console.log(num4)
+}
+
+console.log(num5)
+
+num = 5
+
+console.log(num)
