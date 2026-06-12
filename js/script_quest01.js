06b50caad0b77512a26e9969bb21b4c251e51b35
Marcos <marcos.k.santos6@aluno.senai.br>
2026-06-12 16:23:33 -0300

    INDIVIDUALIZAÇÃO DAS PASTAS DE ATIVIDADE

---
+++ js/script_quest01.js
@@ -0,0 +1,40 @@
+//ATIVIDADE DE LOGÍSTICA - 1#
+
+var num1 = 20;
+var num2 = 26;
+var num3 = 40;
+console.log('MÉDIA ' ,(num1 + num2 + nu) / 3)
+
+//ATIVIDADE DE LOGÍSTICA - 2#
+let altura = 4
+let largura = 5
+let tinta = 2
+console.log('ÁREA DA PAREDE: ' , altura * largura)
+console.log('QUANTIDADE DE TINTA NECESSARIA: ', (altura * largura) / tinta)
+
+//ATIVIDADE DE LOGÍSTICA - 3#
+let distância = 1000
+let consumo = 25
+let litro = 7
+console.log('QUANTIDADE DE COMBUSTÍVEL NECESSÁRIO: ' , distância / consumo)
+console.log('QUANTIDADE À SE PAGAR ' , (distância / consumo)* litro)
+
+//ATIVIDADE DE LOGÍSTICA - 4#
+ console.log("MARCOS")
+ console.log("NOTAS SEMESTRAIS:")
+
+ var NOTA1 = 7
+ var NOTA2 = 5
+ var NOTA3 = 5
+let MÉDIA = ((NOTA1 + NOTA2 + NOTA3) / 3) 
+
+ console.log('NOTA 1 =' , NOTA1)
+ console.log('NOTA 2 =' , NOTA2)
+ console.log('NOTA 3 =' , NOTA3)
+ console.log('MÉDIA =' , MÉDIA)
+ 
+ if(MÉDIA >= 6){
+    console.log('-VOCÊ FOI APROVADO-')
+ }else{
+    console.log('-VOCÊ FOI REPROVADO-')
+ }
