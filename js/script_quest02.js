82ced27347a1522cdaee8d754af21779873683f3
Tarde Cetafaju <Tarde.Cetafaju@labfies.org.br>
2026-06-15 13:56:38 -0300

    Calculo da parede criado

--- js/script_quest02.js
+++ js/script_quest02.js
@@ -14,7 +14,8 @@ let n2 = parseFloat(objFormDados.get('num2'))
 let area = n1 * n2 
 let tinta = 2
 
-divResultado.innerHTML = `A área da parede é: ${area.toFixed(0).replace('.',',')}m`
+divResultado.innerHTML = `A área da parede é: ${area.toFixed(0).replace('.',',')}m , Total de litros  para pintar essa 
+área é de: ${parseFloat(area / tinta).toFixed(0).replace('.','.')}l`
 
 formDados.reset()
 }) 
