636d4365a87472fff41059fb066e19148a76ceea
Tarde Cetafaju <Tarde.Cetafaju@labfies.org.br>
2026-06-15 14:42:10 -0300

    Calculo da media finalizado e trindulo iniciado

--- js/script_quest04.js
+++ js/script_quest04.js
@@ -13,8 +13,18 @@ let nota2 = parseFloat(objFormDados.get('nota2'))
 let nota3 = parseFloat(objFormDados.get('nota3'))
 
 let media = (nota1 + nota2 + nota3)/ 3
+if (media < 6){
+    situação = 'APROVADO'
+}else{
+    situação = 'REPROVADO'
+}
 
-divResultado.innerHTML = `A MÉDIA DO ALUNO É: ${media.toFixed(1).replace('.',',')}`
+
+divResultado.innerHTML = `A MÉDIA DO ALUNO É: ${media.toFixed(1).replace('.',',')}<br>
+${situação}`
+
+
+
 
 formDados.reset()
 }) 
